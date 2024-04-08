6315d660b018348bd84d425af46f2afd64f05b4b
spselenim <pawal.satish@gmail.com>
2024-04-08 17:29:11 +0530

    SP

--- QAFramework/src/test/java/QAClick/QAClickHome.java
+++ QAFramework/src/test/java/QAClick/QAClickHome.java
@@ -32,7 +32,7 @@ public class QAClickHome {
 	{
 		try
 		{
-//Added comments
+//Added comments - SP
 			WebDriverManager.chromedriver().setup();
 			driver=new ChromeDriver();
 			//driver.get("file:///D:/SeleniumAutomation/05DemoApps/demo.html");
